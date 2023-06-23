e14fb1022a971544d4fa118ce20fc7a3594577ee
robinmulders <1190447@student.roc-nijmegen.nl>
2023-06-23 09:51:56 +0200

    kaaskroketjes
    yeah

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
+import reactLogo from "./roclogo.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a>test</a>
+        <div className='headerdesigndriehoek'></div>
+
+        <div className='headerlogo'>
+          <img src={reactLogo} alt="react logo" height="35px" width="100%"/>
+        </div>
+
+      
+        
+        
       </header>
+      <body>
+        <div className='bodysection1container'>
+
+          <div className='schoolinfosection'></div>
+
+          <div className='inlogblok'></div>
+        </div>
+
+      </body>
     </div>
   );
 }
